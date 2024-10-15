d620c43ba31d2e0f5e944c5da07d7ddc0b0f15a8
muhammadjon8 <90293805+muhammadjon8@users.noreply.github.com>
2024-10-15 21:50:36 +0500

    swagger config moved from main.ts to config folder

--- events-api/src/main.ts
+++ events-api/src/main.ts
@@ -4,6 +4,7 @@ import { ConfigService } from '@nestjs/config';
 import { EnvConfig } from './shared/configs/env.config';
 import { ValidationConfig } from './shared/configs/validation.config';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { config } from './shared/configs/swagger.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -15,10 +16,6 @@ async function bootstrap() {
   const port = configService.getOrThrow('APP_PORT', {
     infer: true,
   });
-  const config = new DocumentBuilder()
-    .setTitle('Ticketing API')
-    .setDescription('API for managing tickets').
-    setVersion('1.0').addBearerAuth().build();
 
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api/docs', app, document);
