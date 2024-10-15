99b71b13b2ff5ff63306e013d28317d6cc6d97e6
muhammadjon8 <90293805+muhammadjon8@users.noreply.github.com>
2024-10-15 21:53:22 +0500

    #

--- events-api/src/modules/auth/auth.controller.ts
+++ events-api/src/modules/auth/auth.controller.ts
@@ -4,15 +4,15 @@ import { RegisterDto } from './dto/requests/register.dto';
 import { AuthService } from './auth.service';
 import { LoginDto } from './dto/requests/login.dto';
 
-@ApiTags('Authentication') // Grouping the endpoints under "Authentication" in Swagger
+@ApiTags('Authentication')
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('register')
-  @ApiOperation({ summary: 'Register a new user' }) // Describes the endpoint
-  @ApiResponse({ status: 201, description: 'User successfully registered' }) // Success response
-  @ApiResponse({ status: 400, description: 'Validation failed or user already exists' }) // Error response
+  @ApiOperation({ summary: 'Register a new user' }) 
+  @ApiResponse({ status: 201, description: 'User successfully registered' }) 
+  @ApiResponse({ status: 400, description: 'Validation failed or user already exists' })
   async register(@Body() registerDto: RegisterDto) {
     const createdUser = await this.authService.register(registerDto);
 
@@ -23,9 +23,9 @@ export class AuthController {
   }
 
   @Post('login')
-  @ApiOperation({ summary: 'Login a user' }) // Describes the login operation
-  @ApiResponse({ status: 200, description: 'User successfully logged in' }) // Success response
-  @ApiResponse({ status: 401, description: 'Invalid credentials' }) // Error response for failed login
+  @ApiOperation({ summary: 'Login a user' }) 
+  @ApiResponse({ status: 200, description: 'User successfully logged in' }) 
+  @ApiResponse({ status: 401, description: 'Invalid credentials' }) 
   async login(@Body() loginDto: LoginDto) {
     const authData = await this.authService.login(loginDto);
 
