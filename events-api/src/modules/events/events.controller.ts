fe30833d46d96c83e1507153f1a6291bd771ae27
SurajSulten <suraj.sulten@gmail.com>
2024-10-17 16:31:24 +0500

    feat: create get, patch, delete events methods

--- events-api/src/modules/events/events.controller.ts
+++ events-api/src/modules/events/events.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, HttpStatus, Param, ParseUUIDPipe, Post, Query, UseGuards } from '@nestjs/common';
+import { BadRequestException, Body, Controller, Delete, Get, HttpStatus, Param, ParseUUIDPipe, Patch, Post, Query, UseGuards } from '@nestjs/common';
 import { EventsService } from './events.service';
 import { CreateEventReqDto } from './dto/requests';
 import { ListEventsParamsReqDto } from './dto/requests/list-events-params.dto';
@@ -46,5 +46,38 @@ export class EventsController {
     return {
       data: foundEvent,
     };
+  }
+  @Get()
+  async getFilteredEvents(
+    @Query('sort_by') sortBy: string = 'eventCreatedAt',
+    @Query('sort_order') sortOrder: string = 'asc'
+  ) {
+    const allowedSortBy = ['eventName', 'eventDate', 'eventCreatedAt', 'eventUpdatedat'];
+    const allowedSortOrder = ['asc', 'desc'];
+
+    if(!allowedSortBy.includes(sortBy)) {
+      throw new BadRequestException('Invalid sort by parameter')
+    }
+
+    if(!allowedSortOrder.includes(sortOrder)) {
+      throw new BadRequestException('Invalid sort order parameter')
+    }
+
+    return this.eventsService.getSortedEvents(sortBy, sortOrder);
+  }
+
+  @Patch(':eventId')
+  updateEvent(
+    @Param('eventId') id: string,
+    @Body()updateEventDto: CreateEventReqDto,
+  ){
+    return this.eventsService.updateEvent(id, updateEventDto)
+  }
+
+  @Delete(':eventId')
+  deleteEvent(
+    @Param('eventId') id: string,
+  ){
+    return this.eventsService.deleteEvent(id);
   }
 }
