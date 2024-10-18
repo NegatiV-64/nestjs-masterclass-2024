719e190c6f9a48bfc19d24b41e5a939915224e21
tiescl <anvar.madvaliev@icloud.com>
2024-10-18 23:09:44 +0500

    fix: no need to coerce numbers with class-transformer

--- events-api/src/modules/tickets/dto/requests/create-ticket.dto.ts
+++ events-api/src/modules/tickets/dto/requests/create-ticket.dto.ts
@@ -1,40 +1,11 @@
-import { Transform } from "class-transformer";
 import { IsInt, IsNumber, IsPositive, IsString, Max } from "class-validator";
 
 export class CreateTicketReqDto {
-    @Transform(
-        (params) => {
-            const { value } = params;
-
-            if (typeof value !== "string") {
-                return undefined;
-            }
-
-            return Number(value);
-        },
-        {
-            toClassOnly: true
-        }
-    )
     @IsInt()
     @IsPositive()
     @Max(100000)
     ticketQuantity: number;
 
-    @Transform(
-        (params) => {
-            const { value } = params;
-
-            if (typeof value !== "string") {
-                return undefined;
-            }
-
-            return Number(value);
-        },
-        {
-            toClassOnly: true
-        }
-    )
     @IsNumber({
         allowNaN: false,
         allowInfinity: false,
