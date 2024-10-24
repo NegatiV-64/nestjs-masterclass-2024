61e29d7b81ea5d29658d90329932052f04c37716
tiescl <anvar.madvaliev@icloud.com>
2024-10-24 12:04:43 +0500

    feat: create a custom pipe for uuid validation

--- events-api/src/modules/events/events.controller.ts
+++ events-api/src/modules/events/events.controller.ts
@@ -5,6 +5,7 @@ import { ListEventsParamsReqDto } from './dto/requests/list-events-params.dto';
 import { AuthTokenGuard } from 'src/shared/guards/auth-token.guard';
 import { Roles, RolesGuard } from 'src/shared/guards/roles.guard';
 import { UserRole } from 'src/shared/constants/user-role.constant';
+import { UUIDV4Pipe } from 'src/shared/pipes/parse-v4-uuid.pipe';
 
 @Controller('events')
 export class EventsController {
@@ -32,13 +33,7 @@ export class EventsController {
 
   @Get(':eventId')
   async getEventById(
-    @Param(
-      'eventId',
-      new ParseUUIDPipe({
-        errorHttpStatusCode: HttpStatus.BAD_REQUEST,
-        version: '4',
-      }),
-    )
+    @Param('eventId', UUIDV4Pipe)
     eventId: string,
   ) {
     const foundEvent = await this.eventsService.getEventById(eventId);
@@ -53,13 +48,7 @@ export class EventsController {
   @UseGuards(AuthTokenGuard, RolesGuard)
   public async updateEvent(
     @Body() dto: UpdateEventReqDto,
-    @Param(
-      'eventId',
-      new ParseUUIDPipe({
-        errorHttpStatusCode: HttpStatus.BAD_REQUEST,
-        version: '4',
-      }),
-    )
+    @Param('eventId', UUIDV4Pipe)
     eventId: string,
   ) {
     const updatedEvent = await this.eventsService.updateEvent(eventId, dto);
@@ -73,13 +62,7 @@ export class EventsController {
   @Roles(UserRole.Admin)
   @UseGuards(AuthTokenGuard, RolesGuard)
   public async deleteEvent(
-    @Param(
-      'eventId',
-      new ParseUUIDPipe({
-        errorHttpStatusCode: HttpStatus.BAD_REQUEST,
-        version: '4',
-      }),
-    )
+    @Param('eventId', UUIDV4Pipe)
     eventId: string,
   ) {
     const deletedEvent = await this.eventsService.deleteEvent(eventId);
